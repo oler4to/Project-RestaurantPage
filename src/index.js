8ac5b63ea137342809834a0f76ffcd36355261d9
Olerato. <olerato.mosikidi@gmail.com>
2023-12-02 19:58:17 +0200

    Create IIFE 'createHeader' that will create a header and append to HTML. Create function 'createHome', that will create a home tab. The home tab will contain elements created in home.js module.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import './style.css'
+import homedetails from './home.js'
 
-console.log("Hello, World!")
+const restaurantPage = document.getElementById('restaurant-page');
+
+const createHeader = (() => {
+  const header = document.createElement('header');
+        header.setAttribute('id', 'head');
+  
+  //HEAD
+  const restaurantName = document.createElement('span');
+        restaurantName.setAttribute('id', 'restaurant-name');
+        restaurantName.textContent = 'SAVOUR HAVEN';
+  
+  //TABS
+  const navtabs = document.createElement('div');
+        navtabs.setAttribute('id', 'navtabs');
+        
+    const homeTab = document.createElement('span');
+    const menuTab = document.createElement('span');
+    const contactTab = document.createElement('span');
+    
+    homeTab.textContent = "Home";
+    menuTab.textContent = "Menu";
+    contactTab.textContent = "Contact";
+  
+  navtabs.appendChild(homeTab);
+  navtabs.appendChild(menuTab);
+  navtabs.appendChild(contactTab);
+  
+  header.appendChild(restaurantName);
+  header.appendChild(navtabs);
+  
+  restaurantPage.appendChild(header)
+})()
+
+let currentTab = 'home';
+let setCurrentTab = ''
+
+function createHome(){
+  const hometab = document.createElement('section');
+        hometab.setAttribute('id', 'home');
+    
+  homedetails(hometab);
+  restaurantPage.appendChild(hometab);
+}
+
+createHome()

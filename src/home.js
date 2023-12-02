8bd06965bc5c406c58cb8277e606b43d20e1d6a3
Olerato. <olerato.mosikidi@gmail.com>
2023-12-02 19:59:15 +0200

    Create elements that will go on the home tab of the Restaurant Page.

---
+++ src/home.js
@@ -0,0 +1,37 @@
+import TableImage from './table.jpg';
+import WaitressImage from './waiteress.jpg';
+
+function homedetails(a) {
+  const header = document.createElement('span');
+        header.setAttribute('class', 'tabHead');
+        header.textContent = 'Home'
+        
+  const intro = document.createElement('p');
+        intro.setAttribute('class', 'para');
+        intro.textContent = 'Welcome to "Savor Haven," where culinary artistry meets a welcoming ambiance. Nestled in the heart of the city, our restaurant offers a fusion of flavors crafted from locally sourced ingredients and inspired by global cuisine.';
+        
+  const table = new Image();
+        table.src = TableImage;
+        table.setAttribute('class', 'image');
+        
+  const para2 = document.createElement('p');
+        para2.setAttribute('class', 'para');
+        para2.textContent = "Step into an inviting space adorned with warm hues and contemporary design, where each detail, from the flickering candlelight to the soothing background music, is curated to elevate your dining experience.";
+  
+  const waitress = new Image();
+        waitress.src = WaitressImage;
+        waitress.setAttribute('class', 'image');
+        
+  const para3 = document.createElement('p');
+        para3.setAttribute('class', 'para');
+        para3.textContent = `Our menu is a symphony of delectable options, catering to diverse palates and dietary preferences, ensuring an unforgettable culinary journey. Whether you're seeking an intimate dinner for two or a celebration with loved ones, our attentive staff is dedicated to providing impeccable service, making every visit to "Savor Haven" a feast for the senses and a cherished memory.`
+    
+    a.appendChild(header);
+    a.appendChild(intro);
+    a.appendChild(table);
+    a.appendChild(para2);
+    a.appendChild(waitress);
+    a.appendChild(para3);
+}
+
+export default homedetails

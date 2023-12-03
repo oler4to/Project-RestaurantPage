cfffab02de094948c2efa0a2851108835b37b5a4
Olerato. <olerato.mosikidi@gmail.com>
2023-12-03 16:12:25 +0200

    Add checkCurrentTab(), removeChild() and switchTabs(). These will help implement the logic behind switching between tabs of the site.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import home from './home.js'
 import menu from './menu.js'
 import contact from './contact.js'
 
-
 const restaurantPage = document.getElementById('restaurant-page');
 
 const createHeader = (() => {
@@ -19,13 +18,19 @@ const createHeader = (() => {
   const navtabs = document.createElement('div');
         navtabs.setAttribute('id', 'navtabs');
         
-    const homeTab = document.createElement('span');
-    const menuTab = document.createElement('span');
-    const contactTab = document.createElement('span');
+    const homeTab = document.createElement('button');
+    const menuTab = document.createElement('button');
+    const contactTab = document.createElement('button');
+    
+          homeTab.setAttribute('name', 'home');
+          menuTab.setAttribute('name', 'menu');
+          contactTab.setAttribute('name', 'contact');
     
     homeTab.textContent = "Home";
     menuTab.textContent = "Menu";
     contactTab.textContent = "Contact";
+    
+    homeTab.style = "border-bottom-style: solid";
   
   navtabs.appendChild(homeTab);
   navtabs.appendChild(menuTab);
@@ -36,36 +41,76 @@ const createHeader = (() => {
   
   restaurantPage.appendChild(header)
 })()
-
-let currentTab = 'home';
-let setCurrentTab = ''
-
 function createHome(){
   const hometab = document.createElement('section');
         hometab.setAttribute('id', 'home');
-    
+  
   home(hometab);
   restaurantPage.appendChild(hometab);
+  
+  hometab.style.display = 'flex';
 }
-
-createHome()
-
 function createMenu(){
+  
   const menutab = document.createElement('section');
         menutab.setAttribute('id', 'menu');
   
   menu(menutab);
   restaurantPage.appendChild(menutab)
+  
+  menutab.style.display = "flex";
 }
-
-createMenu()
-
 function createContact(){
+  
   const contacttab = document.createElement('section');
         contacttab.setAttribute('id', 'contact');
         
   contact(contacttab)
   restaurantPage.appendChild(contacttab)
+  
+  contacttab.style.display = "flex";
 }
 
-createContact()
+const tabs = document.querySelectorAll('#navtabs button');
+let currentTab = 'home';
+  
+function removeChild(){
+  restaurantPage.removeChild(restaurantPage.children[1])
+}
+function checkCurrentTab(b){
+    switch (b){
+      case 'home': 
+        tabs[0].style = "border-bottom-style: solid;";
+        tabs[2].style = "border-bottom-style: none;";
+        tabs[1].style = "border-bottom-style: none;";
+        createHome();
+        break;
+      case 'menu':
+        tabs[1].style = "border-bottom-style: solid;";
+        tabs[0].style = "border-bottom-style: none;";
+        tabs[2].style = "border-bottom-style: none;";
+        createMenu();
+        break;
+      case 'contact':
+        tabs[2].style = "border-bottom-style: solid;";
+        tabs[1].style = "border-bottom-style: none;";
+        tabs[0].style = "border-bottom-style: none;";
+        createContact();
+        break;
+      default:
+      return false
+    }
+}
+
+const switchTabs = (() =>{
+  
+createHome()
+
+    tabs.forEach((tab) =>{
+      tab.addEventListener('click', () =>{
+        removeChild(),
+        currentTab = tab.name,
+        checkCurrentTab(currentTab)
+        })
+      })
+})()
